32109188a7a8fc4c721be4eb6855b6273ce6b680
showryareddy <sreddy00999@gmail.com>
2026-05-24 15:37:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 import streamlit as st
+import cv2
+import numpy as np
+from PIL import Image
 
 st.title("Accident Recommendation System")
-st.success("Deployment successfully!")
 
+uploaded_file = st.file_uploader("Upload an accident image", type=["jpg","png","jpeg"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image")
+
+    img_array = np.array(image)
+
+    st.success("Image uploaded successfully")
+
+    # Add your prediction model here
